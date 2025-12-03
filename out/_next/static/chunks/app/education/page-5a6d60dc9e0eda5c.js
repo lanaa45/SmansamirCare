3cc57e86195a4b07b94c8d55379b1d2fe8247769
lanaa45 <maullana.lif@gmail.com>
2025-12-03 17:01:30 +0700

    Add files via upload

---
+++ out/_next/static/chunks/app/education/page-5a6d60dc9e0eda5c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[651,974],{60176:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,6874,23)),Promise.resolve().then(n.t.bind(n,33063,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[63,874,441,684,358],()=>s(60176)),_N_E=e.O()}]);
